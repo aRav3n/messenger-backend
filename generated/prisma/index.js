ce0d422cc166255323bdfb9c13daa88edb4172d5
aRav3n <24660772+aRav3n@users.noreply.github.com>
2025-04-21 14:00:18 -0300

    build: set up dev database

--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -165,11 +165,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://andy:littleann@localhost:5432/messenger_test"
+        "value": null
       }
     }
   },
