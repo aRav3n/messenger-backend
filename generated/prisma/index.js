386df5c60c265b7841978d7b6d17ed49dbd0b0ae
aRav3n <24660772+aRav3n@users.noreply.github.com>
2025-05-05 16:16:50 -0300

    build: set up Prisma for the cloud

--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -165,12 +165,11 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": null
+        "value": "postgresql://neondb_owner:npg_iIe6fHcD3pJr@ep-dry-lake-a4h4k26k-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
       }
     }
   },
